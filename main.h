9140767153af80339a3c317b12367af05c5939d5
nguyennhutninh <nguyennhutninh@gmail.com>
2018-01-31 15:33:02 +0700

    0

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <time.h>
+
+#include "timer.h"
+#include "pid.h"
+#include "I2Cclass.h"
+#include "SPIClass.h"
+#include "dmp.h"
+#include "socket_api.h"
+
+#endif
